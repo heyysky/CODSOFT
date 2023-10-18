f9d58e36dfd738df6bf715e2607d92f4449c47a2
Sourabh Kumar <sourabh.12183.kvsrodelhi@gmail.com>
2023-10-18 20:42:22 +0530

    third commit

---
+++ GradeCalculator.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner ad = new Scanner(System.in);
+
+        System.out.print("Enter number of subjects: ");
+        int numSubjects = ad.nextInt();
+
+        int totalMarks = 0;
+        for (int i = 1; i <= numSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + i + ": ");
+            int marks = ad.nextInt();
+            totalMarks += marks;
+        }
+
+        double averagePercentage = (double) totalMarks / numSubjects;
+
+        String grade;
+        if (averagePercentage >= 90) {
+            grade = "A+";
+        } else if (averagePercentage >= 80) {
+            grade = "A";
+        } else if (averagePercentage >= 70) {
+            grade = "B+";
+        } else if (averagePercentage >= 60) {
+            grade = "B";
+        } else if (averagePercentage >= 50) {
+            grade = "C+";
+        } else if (averagePercentage >= 40) {
+            grade = "C";
+        } else {
+            grade = "D";
+        }
+
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage);
+        System.out.println("Grade: " + grade);
+    }
+}
+
